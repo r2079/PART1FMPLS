e3c81877a89b493c7f73875f76c618b8bde62cc3
r2079 <rakesh_madupu@yahoo.com>
2020-02-29 00:25:04 +0200

    Adding script related changes

--- scripts/ldp/deploy_config.py
+++ scripts/ldp/deploy_config.py
@@ -1,7 +1,8 @@
 import argparse
+from colorama import Fore
+from colorama import Style
 from napalm import get_network_driver
 driver = get_network_driver('junos')
-
 
 parser = argparse.ArgumentParser(description='Deploy configuration to Junos Devices via Napalm')
 parser.add_argument('-r', action='store', dest='Router', help='Pass the Router Name', type=str, required=True)
@@ -10,18 +11,32 @@ args     = parser.parse_args()
 Router   = args.Router
 Filename = args.Filename
 
-def deploy_config(router, filename):
 
+def colorprint(message):
+    
+    print('\n{} [*]{} {}'.format(Fore.YELLOW,message,Style.RESET_ALL))
+
+def  deploy_config(router, filename):
+
+    #opening the connection to the device 
     device = driver(router, 'lab', 'lab123')
-    device.open()
-    device.load_merge_candidate(filename=filename)
+    try:
+        device.open()
+        colorprint('opened the Device, loading the configuration')
+        device.load_merge_candidate(filename=filename)
+    except:
+        print('Error opening connection to Device')
 
     # viewing the diff
+    colorprint('Difference between Candidate and Active Configuration')
     difference_in_config = device.compare_config()
-    print(difference_in_config)
-
+    if difference_in_config:
+        colorprint(difference_in_config)
+    else:
+        colorprint("No Difference in the config! Will not commit anything!")
     #commiting the config
     device.commit_config()
+    colorprint('Configuration has been commited successfully')
 
 deploy_config(router=Router, filename=Filename)
 
