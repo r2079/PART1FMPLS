2334339cf41514a33ea0a3a1e74f9ee75381c1ee
r2079 <rakesh_madupu@yahoo.com>
2020-02-28 18:27:28 -0500

    adding more varibles to the files

--- scripts/ldp/config_gen.py
+++ scripts/ldp/config_gen.py
@@ -4,14 +4,19 @@ import argparse
 from jinja2 import Template
 
 parser = argparse.ArgumentParser(description='Input YAML file and Router Name to generate configuration')
-parser.add_argument('-y', action='store',dest='interface_yaml',help='Pass the yaml file to yaml_file variable',type=str,required=True)
-parser.add_argument('-r', action='store',dest='router_name',help='Pass the Router Name to router_name variable',type=str,required=True)
+parser.add_argument('-y', action='store',dest='interface_yaml',help='Pass the yaml file ',type=str,required=True)
+parser.add_argument('-r', action='store',dest='router_name',help='Pass the Router Name',type=str,required=True)
+parser.add_argument('-j', action='store',dest='jinja_template',help='Pass Jinja Template name used for this config',type=str,required=True)
 args = parser.parse_args()
+
+
+
 
 interface_yaml = args.interface_yaml
 router_name    = args.router_name+'.cfg'
+jinja_template = args.jinja_template
 
-def output_config(interface_yaml,router_name):
+def output_config(interface_yaml,router_name,jinja_template):
 
     try:
         with open(interface_yaml,'r') as yamlfile:
@@ -19,7 +24,7 @@ def output_config(interface_yaml,router_name):
     except:
         print('Check your YAML file')
     try:    
-        with open("interface_template.j2",'r') as jinjafile:
+        with open(jinja_template,'r') as jinjafile:
             jinja_data = jinjafile.read()
             template = Template(jinja_data)
     except:
@@ -31,5 +36,5 @@ def output_config(interface_yaml,router_name):
     print(final_config)
 
 
-output_config(interface_yaml, router_name)
+output_config(interface_yaml, router_name, jinja_template)
 
